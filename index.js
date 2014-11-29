a4965226d8f01a11e8a0f2ef13280faecaa16bf8
Avenger7x <avenger7x13@gmail.com>
2014-11-29 07:31:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function Nothing(value){
+	return value && value.isMonad ? value : { bind: function(){
+			return Nothing();
+	} };
+}
+
+function Just(value){
+	return value && value.isMonad ? value : { bind: function(morphism){
+			return Maybe(morphism(value));
+	} };
+};
+
+function isNullable(value){
+	return typeof value === 'undefined' || value == null;
+}
+
+function Maybe(value){
+	return (isNullable(value) ? Nothing : Just)(value);
+}
+
+exports.Just = Just;
+exports.Nothing = Nothing;
+exports.Maybe = Maybe;
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Maybe
+
+Naive Maybe monad implementation.
+
+# Install
+
+	npm install alexeyraspopov/maybe
+
+# Usage
+
+Simple validation
+
+	function square(n){
+		return n * n;
+	};
+
+	function isEven(n){
+		return n % 2 ? Nothing() : Just(5);
+	}
+
+	Just(5)
+		.bind(square)
+		.bind(isEven)
+		.bind(alert);
+
+Null safe
+
+	Maybe(product.description)
+		.bind(toLowerCase)
+		.bind(translate)
+		.bind(alert);
+
+# License
+
+MIT License &copy; Alexey Raspopov
